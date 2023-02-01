a1b94678bc8fa2842714414a5dda8f835b857d4c
AndreaMinelli <minelliandrea94@gmail.com>
2023-02-01 11:36:17 +0100

    Impostata SectionBase

--- src/components/AppMain.vue
+++ src/components/AppMain.vue
@@ -1,17 +1,84 @@
 <script>
 import NavbarBase from "./NavbarBase.vue";
+import SectionBase from "./SectionBase.vue";
 import { mainNavLinks } from "../data";
 export default {
   name: "AppMain",
-  components: { NavbarBase },
+  components: { NavbarBase, SectionBase },
   data() {
     return {
       mainNavLinks,
+      sectionTutoring: {
+        id: "tutoring",
+        title: "The Trusted Name\nfor In-Home Tutoring.",
+        subTitle:
+          "Lorem ipsum gravida nibh vel velit auctor aliquetnean sollicitudin, lorem quis bibendum auci elit consequat ipsutis sem nibh id eis sed odio sit amet nibh vulputate cursus mauris.",
+        image: "h5-img-1.jpg",
+        rowClasses: ["row-cols-1"],
+      },
+      sectionStories: {
+        id: "user-stories",
+        title: "Empowering\nChildren to Reach\nTheir Potential.",
+        subTitle:
+          "Lorem ipsum gravida nibh vel velit auctor aliquetnean sollicitudin, lorem quis bibendum auci elit consequat ipsutis sem nibh id eis sed odio sit amet nibh vulputate.",
+        image: "h5-img-2.jpg",
+        rowClasses: ["row-cols-2", "flex-row-reverse"],
+        colClasses: ["px-3", "mt-5"],
+        figureClasses: ["pt-5"],
+      },
     };
   },
 };
 </script>
 
 <template>
-  <navbar-base :links="mainNavLinks" listType="cs-card"></navbar-base>
+  <div class="container">
+    <navbar-base :links="mainNavLinks" listType="cs-card"></navbar-base>
+  </div>
+  <section-base :section="sectionTutoring">
+    <template #more-info>
+      <div class="info">
+        <a href="#"> LEARN MORE</a>
+      </div>
+    </template>
+  </section-base>
+  <section-base :section="sectionStories">
+    <template #more-info>
+      <div class="info d-flex">
+        <div>
+          <p class="numbers">168</p>
+          <p class="numbers-info">User Stories</p>
+        </div>
+        <div>
+          <p class="numbers">347</p>
+          <p class="numbers-info">Events</p>
+        </div>
+      </div>
+    </template>
+  </section-base>
 </template>
+
+<style lang="scss" scoped>
+@use "../assets/scss/variables" as *;
+
+.info {
+  color: $dodgerblue-text;
+  font-weight: bolder;
+  letter-spacing: 2px;
+  margin: 15px 0;
+
+  .numbers {
+    font-family: $secondary-font;
+    font-size: 60px;
+    font-weight: bolder;
+    margin-right: 200px;
+    margin-bottom: 0;
+  }
+
+  .numbers-info {
+    font-family: $secondary-font;
+    font-size: 20px;
+    margin-bottom: 0;
+  }
+}
+</style>
---
+++ src/components/SectionBase.vue
@@ -0,0 +1,54 @@
+<script>
+export default {
+  name: "SectionBase",
+  props: {
+    section: Object,
+  },
+  computed: {
+    buildImageUrl() {
+      return new URL(
+        `../assets/img/section_image/${this.section.image}`,
+        import.meta.url
+      ).href;
+    },
+  },
+};
+</script>
+
+<template>
+  <section :id="section.id">
+    <div class="container">
+      <div class="row" :class="section.rowClasses">
+        <div class="col my-4" :class="section.colClasses">
+          <h2 :class="section.titleClasses">{{ section.title }}</h2>
+          <p>{{ section.subTitle }}</p>
+          <slot name="more-info"></slot>
+        </div>
+        <div v-if="section.image" class="col" :class="section.colClasses">
+          <figure :class="section.figureClasses">
+            <img :src="buildImageUrl" alt="Section pics" />
+          </figure>
+        </div>
+      </div>
+      <slot></slot>
+    </div>
+  </section>
+</template>
+
+<style lang="scss" scoped>
+@use "../assets/scss/variables" as *;
+h2 {
+  font-family: $secondary-font;
+  font-size: 40px;
+  font-weight: bolder;
+  white-space: pre-wrap;
+  margin-bottom: 30px;
+  line-height: 60px;
+}
+
+p {
+  font-size: 25px;
+  color: #808080;
+  line-height: 35px;
+}
+</style>
