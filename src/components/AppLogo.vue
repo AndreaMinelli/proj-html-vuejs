3cf3f0de34c79974b651c622520cdd6f935858c3
AndreaMinelli <minelliandrea94@gmail.com>
2023-02-02 16:30:02 +0100

    Aggiunto effetto all'hover logo

--- src/components/AppLogo.vue
+++ src/components/AppLogo.vue
@@ -9,3 +9,10 @@ export default {
     <img src="../assets/img/logo-light.png" alt="Logo iAccademy" />
   </figure>
 </template>
+
+<style scoped>
+#logo:hover {
+  filter: opacity(0.8);
+  cursor: pointer;
+}
+</style>
