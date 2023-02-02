58efe8ef80f4162854e7b8fe5d58b063f044408f
AndreaMinelli <minelliandrea94@gmail.com>
2023-02-02 14:33:08 +0100

    Creati moduli AppFooter e FooterTop

---
+++ src/components/AppFooter.vue
@@ -0,0 +1,17 @@
+<script>
+import FooterTop from "./footer_child/FooterTop.vue";
+export default {
+  name: "AppFooter",
+  components: { FooterTop },
+};
+</script>
+
+<template>
+  <footer-top></footer-top>
+</template>
+
+<style scoped>
+footer {
+  background-color: #222222;
+}
+</style>
